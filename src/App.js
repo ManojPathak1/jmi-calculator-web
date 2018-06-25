0813f9effd536f26087cedde8670b644cdbf2091
Manoj Pathak <manoj.pathak1215@gmail.com>
2018-06-25 09:49:37 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className='header'>
+        </div>
+        <div className='body'>
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+        </div>
+        <div className='bottom'>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
