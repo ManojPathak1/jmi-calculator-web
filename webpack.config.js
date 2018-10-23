adae0cf57b6af8c2dd987e1f0b0381b607271bb3
Manoj Pathak <manoj.pathak1215@gmail.com>
2018-10-23 20:54:03 +0530

    build

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'my-first-webpack.bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.txt$/, use: 'raw-loader' }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './public/index.html'})
+  ],
+  mode: 'production'
+};
